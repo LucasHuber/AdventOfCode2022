02ca684f42e75d29ce5a3070746e01456ca13086
Lucas Huber <lucas.huber.1999@gmail.com>
2022-12-06 09:51:57 +0100

    day02 reformating

--- 02/02.py
+++ 02/02.py
@@ -3,71 +3,33 @@ file = open("02/input.txt")
 lines = file.readlines()
 score = 0
 
-
-# for line in lines:
-#     line = line.replace("\n", "")
-#     parts = line.split(" ")
+for line in lines:
+    line = line.replace("\n", "")
+    parts = line.split(" ")
     
-#     match parts[1]:
-#         case "X":
-#             score += 1
-#         case "Y":
-#             score += 2
-#         case "Z":
-#             score += 3
+    match parts[1]:
+        case "X":
+            score += 1
+        case "Y":
+            score += 2
+        case "Z":
+            score += 3
     
-#     if ((parts[0] == "A" and parts[1] == "X") or
-#         (parts[0] == "B" and parts[1] == "Y") or
-#         (parts[0] == "C" and parts[1] == "Z")):
-#         score += 3
+    if ((parts[0] == "A" and parts[1] == "X") or
+        (parts[0] == "B" and parts[1] == "Y") or
+        (parts[0] == "C" and parts[1] == "Z")):
+        score += 3
         
-#     if ((parts[0] == "A" and parts[1] == "Y") or
-#         (parts[0] == "B" and parts[1] == "Z") or
-#         (parts[0] == "C" and parts[1] == "X")):
-#         score += 6
+    if ((parts[0] == "A" and parts[1] == "Y") or
+        (parts[0] == "B" and parts[1] == "Z") or
+        (parts[0] == "C" and parts[1] == "X")):
+        score += 6
 
-# for line in lines:
-#     line = line.replace("\n", "")
-#     parts = line.split(" ")
-#     choice = ""
-    
-#     match parts[1]:
-#         case "X":
-#             match parts[0]:
-#                 case "A":
-#                     choice = "C"
-#                 case "B":
-#                     choice = "A"
-#                 case "C":
-#                     choice = "B"
-#         case "Y":
-#             choice = parts[0]
-#         case "Z":
-#             match parts[0]:
-#                 case "A":
-#                     choice = "B"
-#                 case "B":
-#                     choice = "C"
-#                 case "C":
-#                     choice = "A"
-            
-    
-#     match choice:
-#         case "A":
-#             score += 1
-#         case "B":
-#             score += 2
-#         case "C":
-#             score += 3
-    
-#     if (parts[1] == "Y"):
-#         score += 3
-        
-#     if (parts[1] == "Z"):
-#         score += 6
+print("Part One: {}".format(score))
         
 from rps import RPS
-        
+
+score = 0
 for line in lines:
     line = line.replace("\n", "")
     parts = line.split(" ")
@@ -97,4 +59,4 @@ for line in lines:
     if (parts[1] == "Z"):
         score += 6
         
-print(score)
+print("Part Two: {}".format(score))
