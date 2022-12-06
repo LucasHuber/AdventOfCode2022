344a160671ec97f36912edc392a965f5f845010e
Lucas Huber <lucas.huber.1999@gmail.com>
2022-12-06 08:13:01 +0100

    day06 further comment clarification

--- 06/06.py
+++ 06/06.py
@@ -3,6 +3,8 @@ file = open("06/input.txt")
 line = file.read().replace("\n", "")
 numb = 0
 
+# String indexing Example: <str>[i(inclusive):j(exclusive)]
+
 for i in range(4, len(line)):
     if (len(set(line[i-4:i])) == 4):
         numb = i
