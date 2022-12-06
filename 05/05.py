49291d39ebfa93f4b155677eb2b0a1ab641ae214
Lucas Huber <lucas.huber.1999@gmail.com>
2022-12-06 09:46:18 +0100

    day05 reformating

--- 05/05.py
+++ 05/05.py
@@ -14,17 +14,29 @@ stack[9] = ['Z', 'W', 'M', 'S', 'C', 'D', 'J']
 
 lines = file.read().split("\n")
 
-# for line in lines:
-#     instr = [int(s) for s in line.split() if s.isdigit()]
-#     imove, ifrom, ito = instr[0], instr[1], instr[2]
+for line in lines:
+    instr = [int(s) for s in line.split() if s.isdigit()]
+    imove, ifrom, ito = instr[0], instr[1], instr[2]
     
-#     for i in range(imove):
-#         stack[ito].append(stack[ifrom].pop())
+    for i in range(imove):
+        stack[ito].append(stack[ifrom].pop())
 
-# output = ""
-# for i in range(1, 10):
-#     output += stack[i].pop()
+output = ""
+for i in range(1, 10):
+    output += stack[i].pop()
     
+print("Part One: {}".format(output))
+
+stack[1] = ['B', 'W', 'N']
+stack[2] = ['L', 'Z', 'S', 'P', 'T', 'D', 'M', 'B']
+stack[3] = ['Q', 'H', 'Z', 'W', 'R']
+stack[4] = ['W', 'D', 'V', 'J', 'Z', 'R']
+stack[5] = ['S', 'H', 'M', 'B']
+stack[6] = ['L', 'G', 'N', 'J', 'H', 'V', 'P', 'B']
+stack[7] = ['J', 'Q', 'Z', 'F', 'H', 'D', 'L', 'S']
+stack[8] = ['W', 'S', 'F', 'J', 'G', 'Q', 'B']
+stack[9] = ['Z', 'W', 'M', 'S', 'C', 'D', 'J']
+
 for line in lines:
     instr = [int(s) for s in line.split() if s.isdigit()]
     imove, ifrom, ito = instr[0], instr[1], instr[2]
@@ -39,4 +51,4 @@ output = ""
 for i in range(1, 10):
     output += stack[i].pop()
     
-print(output)
+print("Part Two: {}".format(output))
