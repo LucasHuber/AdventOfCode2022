a10b075876926b86755c867ab2ca18aefec2d3f5
Lucas Huber <lucas.huber.1999@gmail.com>
2022-12-07 14:06:03 +0100

    day07 sync1

---
+++ 07/07.py
@@ -0,0 +1,23 @@
+def get_dirs(current_line: int) -> list(str):
+    output = []
+    
+    pass
+
+def get_dir_size():
+    pass
+
+def process_dir():
+    pass
+
+def process_dir_size():
+    pass
+
+
+file = open("07/input.txt")
+
+lines = file.read().split("\n")
+
+for line in lines:
+    dirs = []
+    if (line == "$ ls"):
+        
